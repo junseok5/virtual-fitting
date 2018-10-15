c7b9b9f38d9750826a498111b0dff39d0665b6c4
junseok5 <vkehrkrj@naver.com>
2018-10-16 00:36:03 +0900

    user page update

--- frontend/src/components/register/RegSeller/RegSeller.js
+++ frontend/src/components/register/RegSeller/RegSeller.js
@@ -9,18 +9,23 @@ const RegSeller = () => (
   <div>
     <div className={cx('_title')}>판매자 회원가입</div>
     <div className={cx('reg-seller-form')}>
+      <div className={cx('_subtitle')}>사업자 등록번호</div>
       <div className={cx('_input-space')}>
         <input className={cx('_input-form')} type="text" name="seller_crn" placeholder="사업자 등록번호" />
       </div>
+      <div className={cx('_subtitle')}>이메일</div>
       <div className={cx('_input-space')}>
         <input className={cx('_input-form')} type="text" name="seller_email" placeholder="이메일 입력" />
       </div>
+      <div className={cx('_subtitle')}>비밀번호</div>
       <div className={cx('_input-space')}>
         <input className={cx('_input-form')} type="password" name="seller_password" placeholder="비밀번호 입력" />
       </div>
+      <div className={cx('_subtitle')}>매니저 이름</div>
       <div className={cx('_input-space')}>
         <input className={cx('_input-form')} type="text" name="seller_name" placeholder="매니저 이름 입력" />
       </div>
+      <div className={cx('_subtitle')}>휴대폰 번호</div>
       <div className={cx('_input-space')}>
         <input className={cx('_input-form')} type="tel" name="seller_phonNum" placeholder="휴대폰 번호 입력" />
       </div>
--- frontend/src/components/register/RegUser/RegUser.js
+++ frontend/src/components/register/RegUser/RegUser.js
@@ -9,15 +9,19 @@ const RegUser = () => (
   <div>
     <div className={cx('_title')}>회원가입</div>
     <div className={cx('reg-user-form')}>
+      <div className={cx('_subtitle')}>이메일</div>
       <div className={cx('_input-space')}>
         <input className={cx('_input-form')} type="email" name="user_email" placeholder="이메일 입력" />
       </div>
+      <div className={cx('_subtitle')}>비밀번호</div>
       <div className={cx('_input-space')}>
         <input className={cx('_input-form')} type="password" name="user_password" placeholder="비밀번호 입력" />
       </div>
+      <div className={cx('_subtitle')}>이름</div>
       <div className={cx('_input-space')}>
         <input className={cx('_input-form')} type="text" name="user_name" placeholder="이름 입력" />
       </div>
+      <div className={cx('_subtitle')}>휴대폰 번호</div>
       <div className={cx('_input-space')}>
         <input className={cx('_input-form')} type="tel" name="user_phonNum" placeholder="휴대폰 번호 입력" />
       </div>
