89c9fd1067a96192c07c40471414ad4941e0b616
junseok5 <vkehrkrj@naver.com>
2018-10-24 13:47:50 +0900

    Register completed

--- frontend/src/containers/register/RegUserContainer.js
+++ frontend/src/containers/register/RegUserContainer.js
@@ -7,6 +7,7 @@ import * as userActions from 'store/modules/user'
 import * as baseActions from 'store/modules/base'
 
 import { withRouter } from 'react-router'
+import regex from 'lib/regex'
 
 class RegUserContainer extends Component {
 
@@ -37,6 +38,25 @@ class RegUserContainer extends Component {
       phoneNum,
       gender
     } = form.toJS()
+
+    // validation
+    if (!regex.email.test(email)) {
+      BaseActions.setModalMessage('잘못된 이메일 형식입니다.')
+      BaseActions.showModal('error')
+      return
+    } else if (!regex.password.test(password)) {
+      BaseActions.setModalMessage('비밀번호는 6자 이상 30자 이하입니다.')
+      BaseActions.showModal('error')
+      return
+    } else if (!regex.displayName.test(displayName)) {
+      BaseActions.setModalMessage('잘못된 이름 형식입니다.')
+      BaseActions.showModal('error')
+      return
+    } else if (!regex.phoneNum.test(phoneNum)) {
+      BaseActions.setModalMessage('잘못된 폰 번호 형식입니다.')
+      BaseActions.showModal('error')
+      return
+    }
 
     try {
       await RegisterActions.localRegisterUser({
--- frontend/src/lib/regex.js
+++ frontend/src/lib/regex.js
@@ -1,20 +1,16 @@
 // Regex definition
-const regex = {
-  email: /^[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*.[a-zA-Z]{2,3}$/i,
-  password: /^[A-Za-z0-9]{6,12}$/,
-  displayName: /^[a-zA-Z0-9ㄱ-힣]{3,12}$/,
-  managerName: /^[a-zA-Z0-9ㄱ-힣]{3,12}$/,
-  phoneNum: /^\d{3}-\d{3,4}-\d{4}$/,
-  companyName: /^[a-zA-Z0-9ㄱ-힣]{1,30}$/
-}
+const email = /^[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*.[a-zA-Z]{2,3}$/i
+const password = /^[A-Za-z0-9]{6,30}$/
+const displayName = /^[a-zA-Z0-9ㄱ-힣]{2,12}$/
+const managerName = /^[a-zA-Z0-9ㄱ-힣]{3,12}$/
+const phoneNum = /^\d{3}-\d{3,4}-\d{4}$/
+const companyName = /^[a-zA-Z0-9ㄱ-힣]{1,30}$/
 
-export default function validation (payload) {
-  return {
-    register () {
-      
-    },
-    login () {
-
-    }
-  }
+export default {
+  email,
+  password,
+  displayName,
+  managerName,
+  phoneNum,
+  companyName
 }
